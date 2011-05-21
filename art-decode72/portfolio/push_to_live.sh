905e1770524e2b47c660a5873afb54577f8da262
hcwiley <cole@wd40too.com>
2011-05-21 15:45:07 -0500

    got the push to live right

--- art-decode72/portfolio/push_to_live.sh
+++ art-decode72/portfolio/push_to_live.sh
@@ -1,9 +1,9 @@
 ssh wd40too@wd40too.com <<'ENDSSH'
-cd /home/wd40too/hg/artist-sites/
+cd /home/wd40too/hg/art-sites/art-decode72/ 
 hg pull
 hg up
-cp -r art-decode72/portfolio/* /home/wd40too/webapps/artist_sites/portfolio/
-cp -r art-decode72/public/* /home/wd40too/webapps/artist_sites_static/
+cp -r portfolio/* /home/wd40too/webapps/artist_sites/portfolio/
+cp -r public/* /home/wd40too/webapps/artist_sites_static/
 cd /home/wd40too/webapps/artist_sites/apache2/bin
 ./restart
 exit
