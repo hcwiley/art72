dad5f1294001e4602cf16d33817bbaafb3738cfb
hcwiley <cole@decode72.com>
2011-06-26 21:57:21 -0500

    is_dev ?

--- art-decode72/portfolio/settings.py
+++ art-decode72/portfolio/settings.py
@@ -7,7 +7,7 @@ import posixpath
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir, os.pardir)))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)))
 
-IS_DEV = False
+IS_DEV = True
 DEBUG = False
 
 DATABASE_ENGINE = 'django.db.backends.mysql'
