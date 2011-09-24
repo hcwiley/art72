05a7641e3f4f658929fc47b4586fe8c3b11e4dfb
Cole Wiley <cole@decode72.com>
2011-09-23 20:06:26 -0500

    starting this repo over with only the django files

--- art-decode72/portfolio/apps/piece/admin.py
+++
@@ -1,7 +0,0 @@
-from piece.models import *
-from django.contrib import admin
-
-admin.site.register(Piece)
-#admin.site.register(PieceForm)
-admin.site.register(MyImage)
-admin.site.register(Series)
--- art-decode72/portfolio/db_from_repos.py
+++
@@ -1,4 +0,0 @@
-import os;
-
-os.system("mysql -u wd40too_artist -p wd40too_artist < patrick.sql")
-
